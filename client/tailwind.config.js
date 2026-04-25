c681d33217761209c0f053df97525ce606b57ff7
Sushant-py <sushantsinha1007@gmail.com>
2026-04-25 23:07:49 +0530

    Moved tailwind config to client

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./client/src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
-
